b6951ece90dcb6a4f66f31d756ebd16bafa2e8d6
eturnerx <eturnerx@gmail.com>
2020-05-07 00:35:41 +1200

    Check all methods point to the correct API - marking some for followup. Moved condenser_api entries to bottom of file.

--- src/api/methods.js
+++ src/api/methods.js
@@ -1,144 +1,153 @@
 export default [
     {
+      // STATUS: Method not on any public api docs. Awaiting clarification.
       "api": "database_api",
       "method": "set_subscribe_callback",
       "params": ["callback", "clearFilter"]
     },
     {
+      // STATUS: Method not on any public api docs. Awaiting clarification.
       "api": "database_api",
       "method": "set_pending_transaction_callback",
       "params": ["cb"]
     },
     {
+      // STATUS: Method not on any public api docs. Awaiting clarification.
       "api": "database_api",
       "method": "set_block_applied_callback",
       "params": ["cb"]
     },
     {
+      // STATUS: Method not on any public api docs. Awaiting clarification.
       "api": "database_api",
       "method": "cancel_all_subscriptions"
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_trending_tags",
       "params": ["afterTag", "limit"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_tags_used_by_author",
       "params": ["author"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_post_discussions_by_payout",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_comment_discussions_by_payout",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_trending",
       "params": ["query"]
     },
     {
+      //STATUS: Does not appear to exist on any public API
       "api": "database_api",
       "method": "get_discussions_by_trending30",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_created",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_active",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_cashout",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_payout",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_votes",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_children",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_hot",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_feed",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_blog",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_comments",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_promoted",
       "params": ["query"]
     },
     {
-      "api": "database_api",
+      "api": "block_api",
       "method": "get_block_header",
       "params": ["blockNum"]
     },
     {
-      "api": "database_api",
+      "api": "block_api",
       "method": "get_block",
       "params": ["blockNum"]
     },
     {
-      "api": "database_api",
+      "api": "account_history_api",
       "method": "get_ops_in_block",
       "params": ["blockNum", "onlyVirtual"]
     },
     {
-      "api": "database_api",
-      "method": "get_state",
-      "params": ["path"]
-    },
-    {
+      //STATUS: Does not exist in current API docs. Consider removing
       "api": "database_api",
       "method": "get_trending_categories",
       "params": ["after", "limit"]
     },
     {
+      "api": "tags_api",
+      "method": "get_trending_tags",
+      "params": ["start_tag", "limit"]
+    },
+    {
+      //STATUS: Does not exist in current API docs. Consider removing
       "api": "database_api",
       "method": "get_best_categories",
       "params": ["after", "limit"]
     },
     {
+      //STATUS: Does not exist in current API docs. Consider removing
       "api": "database_api",
       "method": "get_active_categories",
       "params": ["after", "limit"]
     },
     {
+      //STATUS: Does not exist in current API docs. Consider removing
       "api": "database_api",
       "method": "get_recent_categories",
       "params": ["after", "limit"]
@@ -153,23 +162,11 @@ export default [
     },
     {
       "api": "database_api",
-      "method": "get_chain_properties"
-    },
-    {
-      "api": "database_api",
       "method": "get_feed_history"
     },
     {
       "api": "database_api",
-      "method": "get_current_median_history_price"
-    },
-    {
-      "api": "database_api",
       "method": "get_witness_schedule"
-    },
-    {
-      "api": "database_api",
-      "method": "get_hardfork_version"
     },
     {
       "api": "database_api",
@@ -178,88 +175,32 @@ export default [
     {
       "api": "account_by_key_api",
       "method": "get_key_references",
-      "params": ["key"]
+      "params": ["keys"]
     },
     {
       "api": "database_api",
-      "method": "get_accounts",
-      "params": ["names"]
+      "method": "find_accounts",
+      "params": ["accounts"]
     },
     {
-      "api": "database_api",
-      "method": "get_account_references",
-      "params": ["accountId"]
-    },
-    {
-      "api": "database_api",
-      "method": "lookup_account_names",
-      "params": ["accountNames"]
-    },
-    {
-      "api": "database_api",
-      "method": "lookup_accounts",
-      "params": ["lowerBoundName", "limit"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_account_count"
-    },
-    {
-      "api": "database_api",
-      "method": "get_conversion_requests",
-      "params": ["accountName"]
-    },
-    {
-      "api": "database_api",
+      "api": "account_history_api",
       "method": "get_account_history",
-      "params": ["account", "from", "limit"]
+      "params": ["account", "start", "limit"]
     },
     {
-      "api": "database_api",
-      "method": "get_owner_history",
-      "params": ["account"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_recovery_request",
-      "params": ["account"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_escrow",
-      "params": ["from", "escrowId"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_withdraw_routes",
-      "params": ["account", "withdrawRouteType"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_account_bandwidth",
-      "params": ["account", "bandwidthType"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_savings_withdraw_from",
-      "params": ["account"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_savings_withdraw_to",
-      "params": ["account"]
-    },
-    {
+      //STATUS: Supports more params
       "api": "database_api",
       "method": "get_order_book",
       "params": ["limit"]
     },
     {
+      //STATUS: Does not appear in any public api docs. Consider removing
       "api": "database_api",
       "method": "get_open_orders",
       "params": ["owner"]
     },
     {
+      //STATUS: Does not appear in any public api docs. Consider removing
       "api": "database_api",
       "method": "get_liquidity_queue",
       "params": ["startAccount", "limit"]
@@ -270,7 +211,7 @@ export default [
       "params": ["trx"]
     },
     {
-      "api": "database_api",
+      "api": "account_history_api",
       "method": "get_transaction",
       "params": ["trxId"]
     },
@@ -292,92 +233,51 @@ export default [
     {
       "api": "database_api",
       "method": "verify_account_authority",
-      "params": ["nameOrId", "signers"]
+      "params": ["account", "signers"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_active_votes",
       "params": ["author", "permlink"]
     },
     {
-      "api": "database_api",
-      "method": "get_account_votes",
-      "params": ["voter"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_content",
-      "params": ["author", "permlink"]
-    },
-    {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_content_replies",
       "params": ["author", "permlink"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_discussions_by_author_before_date",
       "params": ["author", "startPermlink", "beforeDate", "limit"]
     },
     {
-      "api": "database_api",
+      "api": "tags_api",
       "method": "get_replies_by_last_update",
       "params": ["startAuthor", "startPermlink", "limit"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_witnesses",
-      "params": ["witnessIds"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_witness_by_account",
-      "params": ["accountName"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_witnesses_by_vote",
-      "params": ["from", "limit"]
-    },
-    {
-      "api": "database_api",
-      "method": "lookup_witness_accounts",
-      "params": ["lowerBoundName", "limit"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_witness_count"
     },
     {
       "api": "database_api",
       "method": "get_active_witnesses"
     },
     {
+      //STATUS: Not in any api docs. Consider removing
       "api": "database_api",
       "method": "get_miner_queue"
     },
     {
-      "api": "database_api",
-      "method": "get_reward_fund",
-      "params": ["name"]
-    },
-    {
-      "api": "database_api",
-      "method": "get_vesting_delegations",
-      "params": ["account", "from", "limit"]
-    },
-    {
+      //STATUS: Does not exist in any public API docs. Consider for removal
       "api": "login_api",
       "method": "login",
       "params": ["username", "password"]
     },
     {
+      //STATUS: Does not exist in any public API docs. Consider for removal
       "api": "login_api",
       "method": "get_api_by_name",
       "params": ["database_api"]
     },
     {
-      "api": "login_api",
+      "api": "database_api",
       "method": "get_version"
     },
     {
@@ -436,14 +336,10 @@ export default [
       "params": ["trx"]
     },
     {
+      //STATUS: Not in any public API docs. Is this an internal thing?
       "api": "network_broadcast_api",
       "method": "broadcast_transaction_with_callback",
       "params": ["confirmationCallback", "trx"]
-    },
-    {
-      "api": "network_broadcast_api",
-      "method": "broadcast_transaction_synchronous",
-      "params": ["trx"]
     },
     {
       "api": "network_broadcast_api",
@@ -451,6 +347,7 @@ export default [
       "params": ["b"]
     },
     {
+      //STATUS: Not in any public API docs. Is this an internal thing?
       "api": "network_broadcast_api",
       "method": "set_max_block_age",
       "params": ["maxBlockAge"]
@@ -526,9 +423,142 @@ export default [
       "method": "get_resource_pool",
       "params": []
     },
+    // The condenser_api calls that have no direct equivalent are listed below
+    // condenser_api is being deprecated
     {
-      "api": "condensor_api",
+      "api": "condenser_api",
+      "method": "broadcast_transaction_synchronous",
+      "params": ["trx"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_accounts",
+      "params": ["account"]
+    },
+    {
+      //STATUS: Probably removed since HF0.20.6 in favour of RC
+      "api": "condenser_api",
+      "method": "get_account_bandwidth",
+      "params": ["account", "type"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_account_count"
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_account_references",
+      "params": ["accountId"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_account_votes",
+      "params": ["voter"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_chain_properties"
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_conversion_requests",
+      "params": ["accountName"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_content",
+      "params": ["author", "permlink"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_current_median_history_price"
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_escrow",
+      "params": ["from", "escrowId"]
+    },
+    {
+      "api": "condenser_api",
       "method": "get_expiring_vesting_delegations",
       "params": ["account","after"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_hardfork_version"
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_owner_history",
+      "params": ["account"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_recovery_request",
+      "params": ["account"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_reward_fund",
+      "params": ["name"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_savings_withdraw_from",
+      "params": ["account"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_savings_withdraw_to",
+      "params": ["account"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_state",
+      "params": ["path"],
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_vesting_delegations",
+      "params": ["account", "from", "limit"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_withdraw_routes",
+      "params": ["account", "withdrawRouteType"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_witnesses",
+      "params": ["witnessIds"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_witness_by_account",
+      "params": ["accountName"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_witnesses_by_vote",
+      "params": ["from", "limit"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "get_witness_count"
+    },
+    {
+      "api": "condenser_api",
+      "method": "lookup_accounts",
+      "params": ["lowerBoundName", "limit"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "lookup_account_names",
+      "params": ["accountNames"]
+    },
+    {
+      "api": "condenser_api",
+      "method": "lookup_witness_accounts",
+      "params": ["lowerBoundName", "limit"]
     }
 ];
